dc441b1f5fb265b4a9ba9fb03d580bad4db57fc5
sahuashi <54184879+sahuashi@users.noreply.github.com>
2020-12-08 23:34:43 -0500

    Started Dynamic Linkage

---
+++ SWENG421FinalProject/AbstractLoadableClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWENG421FinalProject
+{
+    class AbstractLoadableClass
+    {
+        private Company enviroment;
+
+        public void setEnviroment(Company company)
+        {
+            enviroment = company;
+        }
+
+        protected Company getEnviroment()
+        {
+            return enviroment;
+        }
+    }
+}
---
+++ SWENG421FinalProject/Loadable/Drone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWENG421FinalProject.Loadable
+{
+    class Drone : AbstractLoadableClass
+    {
+        int flightTime;
+        int range;
+    }
+}
